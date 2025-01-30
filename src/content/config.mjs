c00dd9ff4456220ea174d1d0448ba7b308bf8ada
Robert Kirk <rk@robertjkirk.com>
2025-01-29 22:12:34 -0600

    lhr video

--- src/content/config.mjs
+++ src/content/config.mjs
@@ -2,44 +2,47 @@ import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
   type: "content", // This is important!
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    formattedDate: z.string(),
-    description: z.string(),
-    coverImage: z.string().optional(),
-    readingTime: z.number(),
-    tags: z.array(z.string()).optional(),
-    featuredMessage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      formattedDate: z.string(),
+      description: z.string(),
+      coverImage: image().optional(),
+      readingTime: z.number(),
+      tags: z.array(z.string()).optional(),
+      featuredMessage: z.string().optional(),
+    }),
 });
 
 const timeline = defineCollection({
   type: "content", // This is important!
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    formattedDate: z.string(),
-    description: z.string().optional(), // optional
-    coverImage: z.string().optional(), // optional
-    readingTime: z.number(),
-    tags: z.array(z.string()).optional(), // optional
-    featuredMessage: z.string().optional(), // optional
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      formattedDate: z.string(),
+      description: z.string().optional(), // optional
+      coverImage: image().optional(), // optional
+      readingTime: z.number(),
+      tags: z.array(z.string()).optional(), // optional
+      featuredMessage: z.string().optional(), // optional
+    }),
 });
 
 const legal = defineCollection({
   type: "content", // This is important!
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    formattedDate: z.string(),
-    description: z.string().optional(), // optional
-    coverImage: z.string().optional(), // optional
-    readingTime: z.number(),
-    tags: z.array(z.string()).optional(), // optional
-    featuredMessage: z.string().optional(), // optional
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      formattedDate: z.string(),
+      description: z.string().optional(), // optional
+      coverImage: image().optional(), // optional
+      readingTime: z.number(),
+      tags: z.array(z.string()).optional(), // optional
+      featuredMessage: z.string().optional(), // optional
+    }),
 });
 
 export const collections = {
